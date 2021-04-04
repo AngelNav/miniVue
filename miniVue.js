c5d1817690c7b40a6d37c6ad8cc601005eabea10
AngelNav <angel98.nava@gmail.com>
2021-04-04 01:05:02 -0600

    Reflect added

--- miniVue.js
+++ miniVue.js
@@ -2,19 +2,51 @@ class MiniVueRective {
     constructor(options) {
         //origin from proxy
         this.origin = options.data()
+
+        //destiny
+
+        //$data is the same as origin but with
+        //methods from proxy (trampas)
+        this.$data = new Proxy(this.origin, {
+            get(target, name) {
+                if (Reflect.has(target, name))
+                    return Reflect.get(target, name)
+
+                console.warn(`Property ${name} doesn't exists`)
+                return ""
+            },
+            set(target, name, value) {
+                Reflect.set(target, name, value)
+            }
+        })
     }
 
+    //effect - track - trigger
     mount() {
         document.querySelectorAll("*[m-text]").forEach(el => {
-            this.mText(el, this.origin, el.getAttribute("m-text"))
+            this.mText(el, this.$data, el.getAttribute("m-text"))
+        })
+
+        document.querySelectorAll("*[m-model]").forEach(el => {
+            const name = el.getAttribute("m-model")
+            this.mModel(el, this.$data, name)
+
+            el.addEventListener("input", () => {
+                //this.$data[name] = el.value
+                Reflect.set(this.$data, name, el.value)
+            })
         })
     }
 
     mText(el, target, name) {
-        el.innerText = target[name]
+        // el.innerText = target[name]
+        //Reflect.get(object, propertyKey)
+        el.innerText = Reflect.get(target, name)
     }
 
-    mModel() { }
+    mModel(el, target, name) {
+        el.value = Reflect.get(target, name)
+    }
 
 }
 
